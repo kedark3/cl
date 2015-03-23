a5afb88f318b850f577d610c983c0045037862bc
Kedar <kedar.kulkarni0@gmail.com>
2015-03-23 22:21:52 +0530

    named pipe

--- working/named pipe/client1.c
+++ working/named pipe/client1.c
@@ -7,7 +7,7 @@
 
 int main()
 {
-    int fd;
+    int fd,ch;
     char * clientfifo = "/tmp/clientfifo";
     char * serverfifo = "/tmp/serverfifo";
     char buf[MAX_BUF];
@@ -15,12 +15,49 @@ int main()
     /* open, read, and display the message from the FIFO */
     for(;;)
     {
+	printf("\n1.OS Name \n2.Kernel Version \n3.OS Distribution");
+	printf("\nEnter your Choice:\t");
+	scanf("%d",&ch);
+	fd = open(clientfifo, O_WRONLY);
+	write(fd, &ch, sizeof(ch));
+	close(fd);
     	fd = open(serverfifo, O_RDONLY);
     	read(fd, buf, MAX_BUF);
-    	printf("Received: %s\n", buf);
+    	printf("We Received: %s\n", buf);
     	close(fd);
     }
     return 0;
 }
 
+/*
+Output:
+sairam@sairam-VPCEH28FN:~$ gcc client1.c -o client
+sairam@sairam-VPCEH28FN:~$ ./client
 
+1.OS Name 
+2.Kernel Version 
+3.OS Distribution
+Enter your Choice:	1
+We Received: Ubuntu 14.04.1 LTS
+
+
+1.OS Name 
+2.Kernel Version 
+3.OS Distribution
+Enter your Choice:	2
+We Received: 3.13.0-43-generic
+
+
+1.OS Name 
+2.Kernel Version 
+3.OS Distribution
+Enter your Choice:	3
+We Received: Linux version 3.13.0-43-generic (buildd@akateko) (gcc version 4.8.2 (Ubuntu 4.8.2-19ubuntu1) ) #72-Ubuntu SMP Mon Dec 8 19:35:44 UTC 2014
+
+
+1.OS Name 
+2.Kernel Version 
+3.OS Distribution
+Enter your Choice:	^C
+sairam@sairam-VPCEH28FN:~$ 
+*/
--- working/named pipe/server1.c
+++ working/named pipe/server1.c
@@ -2,24 +2,52 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include<string.h>
 #include<stdio.h>
-
+#define MAX_BUF 1024
 int main()
 {
-    int fd;
-    char ch;
+    int fd,i;
+    FILE *fp;
     char * serverfifo = "/tmp/serverfifo";
-
+    char * clientfifo = "/tmp/clientfifo";
+    char buf[MAX_BUF];
     /* create the FIFO (named pipe) */
     mkfifo(serverfifo, 0666);
 
-    /* write "Hi" to the FIFO */
-    fd = open(serverfifo, O_WRONLY);
+    
     for(;;)
     {
-    	printf("\n1.Os Name\n2.Kernal Version\n3.OS distribution");
-    	scanf("%c",&ch);
-    	write(fd, &ch, sizeof(ch));
+	fd = open(clientfifo, O_RDONLY);
+    	read(fd, buf, MAX_BUF);
+	close(fd);
+	fd = open(serverfifo, O_WRONLY);    
+	i=buf[0];	
+	switch(i)
+	{
+		case 1:
+		fp=popen("lsb_release -ds","r");
+		fgets(buf,MAX_BUF,fp);
+		printf("\nServer sent: %s",buf);
+		pclose(fp);
+		break;
+		case 2:
+		fp=popen("uname -r","r");
+		fgets(buf,MAX_BUF,fp);
+		printf("\nServer sent: %s",buf);
+		pclose(fp);
+		break;
+		
+		case 3:
+		fp=popen("cat /proc/version","r");
+		fgets(buf,MAX_BUF,fp);
+		printf("\nServer sent: %s",buf);
+		pclose(fp);
+		break;
+		default:strcpy(buf,"Wrong Choice, try again");
+		
+	}
+    	write(fd, &buf, sizeof(buf));
     }    
     close(fd);
     /* remove the FIFO */
@@ -27,3 +55,17 @@ int main()
 
     return 0;
 }
+/*Output:
+sairam@sairam-VPCEH28FN:~$ gcc server1.c -o server
+sairam@sairam-VPCEH28FN:~$ ./server
+
+Server sent: Ubuntu 14.04.1 LTS
+
+Server sent: 3.13.0-43-generic
+
+Server sent: Linux version 3.13.0-43-generic (buildd@akateko) (gcc version 4.8.2 (Ubuntu 4.8.2-19ubuntu1) ) #72-Ubuntu SMP Mon Dec 8 19:35:44 UTC 2014
+^C
+sairam@sairam-VPCEH28FN:~$ 
+
+
+*/
